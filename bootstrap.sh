cd79cf2f193cab6f9d911d6e5593e142030027cd
derevenets_artem <artem@comvoy.be>
2021-01-30 13:19:02 +0200

    Add installing 'exa' package

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,3 +9,10 @@ sudo apt-get install fish
 
 echo -e "### \t Set 'fish' as a default shell \n"
 sudo chsh -s /usr/bin/fish
+
+echo -e "### \t Installing 'exa' package \n"
+sudo apt-get install wget unzip libgit2-dev cmake libhttp-parser-dev
+
+wget https://github.com/ogham/exa/releases/download/v0.9.0/exa-linux-x86_64-0.9.0.zip
+unzip exa-linux-x86_64-0.9.0.zip && rm -rf exa-linux-x86_64-0.9.0.zip
+sudo mv exa-linux-x86_64 /usr/local/bin/exa
