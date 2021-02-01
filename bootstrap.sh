88f9e34b1c45014f62745e6af6ec7b293a70a361
derevenets_artem <artem@comvoy.be>
2021-02-01 19:53:05 +0200

    Add more lines to boostrap.sh output and group packages

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,28 +1,28 @@
-echo -e "### \t Add repositories needed for further system set up \n"
+echo -e "\n### \t Add repositories needed for further system set up \n"
 sudo apt-add-repository ppa:fish-shell/release-3
 
 
-echo -e "### \t Update and upgrade system \n"
+echo -e "\n### \t Update and upgrade system \n"
 sudo apt-get update -y && sudo apt-get upgrade
 
 
-echo -e "### \t Install required packages \n"
-sudo apt-get install fish
+echo -e "\n### \t Install required packages \n"
+sudo apt-get install fish wget unzip cmake
 
 
-echo -e "### \t Set 'fish' as a default shell \n"
+echo -e "\n### \t Set 'fish' as a default shell \n"
 sudo chsh -s /usr/bin/fish
 
 
-echo -e "### \t Installing 'exa' package \n"
-sudo apt-get install wget unzip libgit2-dev cmake libhttp-parser-dev
+echo -e "\n### \t Installing 'exa' package \n"
+sudo apt-get install libgit2-dev libhttp-parser-dev
 
 wget https://github.com/ogham/exa/releases/download/v0.9.0/exa-linux-x86_64-0.9.0.zip
 unzip exa-linux-x86_64-0.9.0.zip && rm -rf exa-linux-x86_64-0.9.0.zip
 sudo mv exa-linux-x86_64 /usr/local/bin/exa
 
 
-echo -e "### \t Installing 'bat' package \n"
+echo -e "\n### \t Installing 'bat' package \n"
 VER="0.17.1"
 wget https://github.com/sharkdp/bat/releases/download/v${VER}/bat_${VER}_amd64.deb
 sudo dpkg -i bat_${VER}_amd64.deb
